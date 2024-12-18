e4577ef8e143bd6ed1ce431a255b9ad3ba08d820
dheerajoruganty <db2winfb@gmail.com>
2024-12-18 05:18:01 +0000

    update folder names

--- data/subreddit_comments/prepare.py
+++ data/subreddit_comments/prepare.py
@@ -12,7 +12,7 @@ import numpy as np
 # download the tiny shakespeare dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'subreddit_comments.txt')
 if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+    data_url = 'https://raw.githubusercontent.com/dheerajoruganty/nanoGPT/refs/heads/master/data/shakespeare_char/subreddit_comments.txt'
     with open(input_file_path, 'w') as f:
         f.write(requests.get(data_url).text)
 
